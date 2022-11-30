7d3d385f19071c8f6b9e4b19838fb74ddd0c7964
CHUM NOEURN <soknoeurn-srun>
2022-11-30 11:26:51 +0700

    #5 add Products by form input

--- pages/seller/js/seller.js
+++ pages/seller/js/seller.js
@@ -28,7 +28,8 @@ let products = [
   }
 ];
 
-
+//get index--------------------
+let index = products.length;
 //HIDE AND SHOW -------------------------------------------
 let hide = (element) => {
   element.style.display = "none";
@@ -73,17 +74,16 @@ let renderProducts = () =>{
     let detail = document.createElement('div');
     detail.className = 'detail';  
     productInfo.appendChild(detail);
-    // console.log(productInfo)
 
     let names = document.createElement('span');
     names.className = 'name';
-    names.textContent = product.name;
+    names.textContent = product.name + ' ' + product.type;
     detail.appendChild(names);
 
-    let type = document.createElement('span');
-    type.className = 'type';
-    type.textContent = product.type;
-    detail.appendChild(type);
+    // let type = document.createElement('span');
+    // type.className = 'type';
+    // type.textContent = product.type;
+    // detail.appendChild(type);
 
     let description = document.createElement('span');
     description.className = 'description';
@@ -92,7 +92,7 @@ let renderProducts = () =>{
 
     let price = document.createElement('span');
     price.className = 'price';
-    price.textContent = product.price + ' ' + product.currency;
+    price.textContent = 'Price: ' + product.price + ' ' + product.currency;
     detail.appendChild(price);
 
     // create a new div for button update and delete-------------
@@ -116,22 +116,29 @@ let renderProducts = () =>{
   product_view.appendChild(dom_product_container);
 
 }
+// let updateProduct = (event) => {
+//   //get product index
+//   productToUpdate = event.target.parentElement.parentElement.dataset.index;
 
+//   // update dialog with product information
+//   let product = products[productToUpdate];
+//   document.
+  
+// }
 let removeProduct = (event) => {
   let index = event.target.parentElement.parentElement.dataset.index;
-  console.log(index);
   // remove product
   products.splice(index, 1);
 
-  //save to locla storage 
+  //save to local storage 
   saveProducts()
 
   //update to the view
   renderProducts()
 }
-let onAdd = () => {
+let onAddProduct = () => {
   show(dom_product_dialog)
-  console.log('yes');
+  document.getElementById('createEditButton').textContent = 'Create';
 };
 //Cancel and Add -------------------------------------------
 let onCancel = (e) =>{
@@ -139,9 +146,43 @@ let onCancel = (e) =>{
   console.log(dom_product_dialog);
   hide(dom_product_dialog);
 }
+let onCreateProduct = () => {
+  let product = document.querySelector('.product_info img');
+  let product_name = document.getElementById('name').value;
+  console.log(product_name);
+  let product_type = document.getElementById('type').value;
+  let product_description = document.getElementById('text').value;
+  console.log(product_description);
+  let product_price = document.getElementById('productPrice').value;
+  console.log(product_price);
+  let currency = document.getElementById('currency').value;
+  console.log(currency);
+  let choose_img = document.getElementById('productImage').value;
+  console.log(choose_img);
 
+  let product_input =  product_name && product_description && product_price && currency && choose_img;
+  if (!(product_input)){
+    // 
+    console.log('false');
+  }else {
+    product_input = {
+      name: product_name,
+      type:product_type,
+      description: product_description,
+      price: product_price,
+      currency: currency,
+    }
+    products.push(product_input);
+    hide(dom_product_dialog);
+  }
+  //save to local storage
+  saveProducts();
+  // update to the view
+  renderProducts()
+}
+saveProducts()
 loadProducts();
 renderProducts();
 
 let btn_add = product_view.querySelector('#add_product');
-btn_add.addEventListener('click', onAdd);
+btn_add.addEventListener('click', onAddProduct);
